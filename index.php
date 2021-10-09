8ac58a3f97c9faa3c39f6259e31bb47452a1c3e0
Vianney187 <vianneyhek187@gmail.com>
2021-10-09 15:01:59 +0200

    modif

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
