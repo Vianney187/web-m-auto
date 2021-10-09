3e0b4f3f661eac4b1064d868069868e0263c0cc6
Vianney187 <vianneyhek187@gmail.com>
2021-10-09 15:11:58 +0200

    modif

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">M Auto | Claudette</a>
+            <a class="navbar-brand" href="#">M Auto </a>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
